f4545758dfe4b34c2e704642c9dadb00e0cba26c
AlexSukov <sukovsasha@gmail.com>
2019-07-30 13:27:11 +0300

    #3: Second and third section (#3)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 jQuery(document).ready(function($) {
   $("#fullpage").fullpage({
     sectionsColor:['#C63D0F','#1BBC9B','#7E8F7C']
-});
+  });
+
+  $('.about-slider').slick({
+    prevArrow: $('.as-prev'),
+    nextArrow: $('.as-next'),
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
 })
