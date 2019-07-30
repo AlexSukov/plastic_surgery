ef2f35f1828aa8b622d48a161853de182b6ea4e8
unknown <sukovsasha@gmail.com>
2019-07-30 16:12:32 +0300

    #5: Rebase 1

--- js/main.js
+++ js/main.js
@@ -24,4 +24,26 @@ jQuery(document).ready(function($) {
       }
     ]
   });
+
+  $('.testimonials-slider').slick({
+    prevArrow: $('.ts-prev'),
+    nextArrow: $('.ts-next'),
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 })
